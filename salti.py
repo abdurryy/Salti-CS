21f249fb19336d81af5f3b25c9ebf427af969a3e
Abdurryy <abdurrahman.giumale@gmail.com>
2023-10-11 15:36:07 +0200

    changed files

--- salti.py
+++ salti.py
@@ -42,7 +42,6 @@ class Salti:
                     response = self.serial.read(self.serial.inWaiting()).decode()
                     if not "VOICE" in response:
                         continue
-                    print(response)
                     if "END" in response:
                         self.log(f"Call to {target} failed", "failure")
                         return 0
@@ -51,8 +50,8 @@ class Salti:
                         self.log(f"Call to {target} successful", "success")
                         return 1
         except Exception as e:
-            self.log(f"err: {str(e)}", "error")
             return 0
+            # self.log(f"err: {str(e)}", "error")
     
     def background(self):
         while True:
@@ -95,7 +94,6 @@ s.on()
 threading.Thread(target=s.background).start()
 while True:
     time.sleep(1)
-    print("status", s.inCall)
     if not s.inCall:
         target = input("Enter number: ")
         if target == "exit":
