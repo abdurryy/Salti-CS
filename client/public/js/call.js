51246ece82affa4ae053acda83671249fc07754c
Abdurryy <abdurrahman.giumale@gmail.com>
2023-10-17 21:52:57 +0200

    a

--- client/public/js/call.js
+++ client/public/js/call.js
@@ -2,7 +2,7 @@ const server_api_url = "http://192.168.1.123:3001/";
 
 async function status() {
     try {
-        const response = await fetch("http://192.168.1.123:3001/status", {
+        const response = await fetch(`${server_api_url}status`, {
             method: "GET",
             mode: "cors",
         });
@@ -12,10 +12,82 @@ async function status() {
         }
 
         const data = await response.json();
-        console.log(data.in_call);
+        callbtn = document.getElementById("call_btn")
+        hangupbtn = document.getElementById("hangup_ctn")
+        hangupgtn = document.getElementById("hangup_btn")
+        if (data.in_call == true) {
+            target = data.call_server_dict.number;
+            color = "";
+            if (data.call_server_dict.status == 1){
+                msg = `CURRENTLY CALLING ${target}`; 
+                color = "orange";
+                hangupgtn.innerHTML = `STOP`;
+
+            } else if (data.call_server_dict.status == 2){
+                msg = `CURRENTLY IN CALL WITH ${target}`;
+                hangupgtn.innerHTML = `HANG UP`;
+                color = "green";
+            } else if (data.call_server_dict.status == 3){
+                msg = `${target} DECLINED THE CALL`;
+                color = "red";
+            } else {
+                msg =`CALL IN QUEUE`;
+                color = "yellow";
+            }
+            
+            callbtn.disabled = true;
+            callbtn.style.backgroundColor = "grey";
+            hangupbtn.style.display = "flex";
+        } else {
+            callbtn.disabled = false;
+            hangupbtn.style.display = "none";
+            callbtn.style.backgroundColor = "#58ff90";
+        }
+        document.getElementById("status").innerHTML =  + data.in_call == false ? "SERVER STATUS: <span style='color:green;'>NOT IN A CALL </span" : `SERVER STATUS: <span style='color:${color};'>${msg}</span>`;
     } catch (error) {
         console.error(error);
     }
 }
 
-status();
+async function hangup() {
+    try {
+        const response = await fetch(`${server_api_url}hangup`, {
+            method: "GET",
+            mode: "cors",
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function call() {
+    try {
+        number = document.getElementById("number").value;
+        const response = await fetch(`${server_api_url}call/${number}`, {
+            method: "GET",
+            mode: "cors",
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.status == "failure"){
+            hangup();
+            document.getElementById("popup").style.display = "flex";
+        }
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+setInterval(() => {
+    status(); 
+}, 100);
+
