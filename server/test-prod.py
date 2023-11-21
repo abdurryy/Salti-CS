8390e22b7649ebc4e5887c76b8e06a155ecf0dcc
Abdurryy <abdurrahman.giumale@gmail.com>
2023-11-21 17:29:34 +0100

    Trying to debug my biggest error! :(

--- server/test-prod.py
+++ server/test-prod.py
@@ -11,55 +11,6 @@ phone_number = '0724438573'
 power_key = 6
 rec_buff = ''
 
-def send_at(command,back,timeout):
-	rec_buff = ''
-	ser.write((command+'\r\n').encode())
-	time.sleep(timeout)
-	if ser.inWaiting():
-		time.sleep(0.01 )
-		rec_buff = ser.read(ser.inWaiting())
-	if back not in rec_buff.decode():
-		print(command + ' ERROR')
-		print(command + ' back:\t' + rec_buff.decode())
-		return 0
-	else:
-		print(rec_buff.decode())
-		return 1
-
-def power_on(power_key):
-	print('SIM7600X is starting:')
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(power_key,GPIO.OUT)
-	time.sleep(0.1)
-	GPIO.output(power_key,GPIO.HIGH)
-	time.sleep(2)
-	GPIO.output(power_key,GPIO.LOW)
-	time.sleep(20)
-	ser.flushInput()
-	print('SIM7600X is ready')
-
-def power_down(power_key):
-	print('SIM7600X is loging off:')
-	GPIO.output(power_key,GPIO.HIGH)
-	time.sleep(3)
-	GPIO.output(power_key,GPIO.LOW)
-	time.sleep(18)
-	print('Good bye')
-
-try:
-	power_on(power_key)
-	send_at('ATD'+phone_number+';','OK',1)
-	time.sleep(20)
-	ser.write('AT+CHUP\r\n'.encode())
-	print('Call disconnected')
-	power_down(power_key)
-except Exception as e:
-    if ser != None:
-        ser.close()
-        GPIO.cleanup()
-    print(f"err: {str(e)}")
-
-if ser != None:
-	ser.close()
-	GPIO.cleanup()
+bytes_recieved = ser.inWaiting()
+response = str(ser.read(ser.inWaiting()).decode("utf-8"))
+print(bytes_recieved)
