a140f3c142c7b1971eb5995939406422182fb0da
Abdurryy <abdurrahman.giumale@gmail.com>
2023-11-24 10:33:34 +0100

    Trying to debug my biggest error! :(

--- server/test-prod.py
+++ server/test-prod.py
@@ -7,9 +7,8 @@ import time
 ser = serial.Serial('/dev/ttyUSB2',115200)
 ser.flushInput()
 
-phone_number = '0724438573'
-power_key = 6
-rec_buff = ''
-
-response = str(ser.read(ser.inWaiting()).decode("utf-8"))
-print(response)
+while True:
+    cmd = input("Enter AT command: ")
+    ser.write((f'ATD{cmd};\r\n').encode())
+    response = str(ser.read(ser.inWaiting()).decode("utf-8"))
+    print(response)
